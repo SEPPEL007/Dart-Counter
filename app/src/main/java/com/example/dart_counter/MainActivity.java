4fd9a9fb759eacf66103325ee962c14928b19513
SEPPEL007 <basti.eberl@googlemail.com>
2020-01-28 11:24:24 +0100

    added gametype and playercount added string.xml

--- app/src/main/java/com/example/dart_counter/MainActivity.java
+++ app/src/main/java/com/example/dart_counter/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.dart_counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button gameModeButton;
+    public Button playerCountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gameModeButton = findViewById(R.id.gameModeButton);
+        playerCountButton = findViewById(R.id.playerCountButton);
+
+        gameModeButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                gameModeChange();
+            }
+        });
+
+        playerCountButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                playerNumberChange();
+            }
+        });
+    }
+
+    //Adds 1 to the player count
+    //returns to 1 when reaching 4
+    public void playerNumberChange(){
+        if (Integer.parseInt(playerCountButton.getText().toString()) == 4){
+            playerCountButton.setText("1");
+        } else {
+            playerCountButton.setText(String.valueOf(Integer.parseInt(playerCountButton.getText().toString()) + 1));
+        }
+    }
+
+    //Adds 200 to the game Type
+    //returns to 301 when reaching 901
+    public void gameModeChange(){
+        if (Integer.parseInt(gameModeButton.getText().toString()) == 901){
+            gameModeButton.setText("301");
+        } else {
+            gameModeButton.setText(String.valueOf(Integer.parseInt(gameModeButton.getText().toString()) + 200));
+        }
     }
 }
